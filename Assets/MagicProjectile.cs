2bfcce4d6ebee414bb9400c302ae7a14be3f02b6
Adam Tovatt <sporedenstore@hotmail.com>
2022-01-02 09:44:20 +0100

    magic projectiles have better targeting

--- Assets/MagicProjectile.cs
+++ Assets/MagicProjectile.cs
@@ -11,29 +11,36 @@ public class MagicProjectile : MonoBehaviour
     private Transform shootingTransform;
     private Vector3 origin;
     private float shootTime;
+    private Transform target;
+    private Vector3 targetPositionHeightOffset;
 
     public void Shoot(Vector3 shootOrigin, Transform shooter)
     {
         Transform[] children = gameObject.GetComponentsInChildren<Transform>();
-        foreach(Transform childTransform in children)
+        foreach (Transform childTransform in children)
         {
             childTransform.localScale = new Vector3(Scale, Scale, Scale);
         }
 
         Health targetHealth = null;
-        float closestTargetHealhtDistance = 10000f;
+        float smallestAngle = 10000f;
 
         if (GameManager.Instance != null)
         {
             Ray sphereCastRay = new Ray(shootOrigin, transform.forward);
-            foreach (RaycastHit hit in Physics.SphereCastAll(sphereCastRay, 4))
+            foreach (RaycastHit hit in Physics.SphereCastAll(sphereCastRay, 6))
             {
                 if (hit.transform != shooter)
                 {
                     if (GameManager.Instance.TransformsWithHealth.ContainsKey(hit.transform))
                     {
-                        if (hit.distance < closestTargetHealhtDistance)
+                        float angle = Vector3.Angle(shooter.forward, (hit.point - shooter.position).normalized);
+                        if (angle < smallestAngle)
+                        {
                             targetHealth = GameManager.Instance.TransformsWithHealth[hit.transform];
+                            target = targetHealth.transform;
+                            smallestAngle = angle;
+                        }
                     }
                 }
             }
@@ -52,10 +59,25 @@ public class MagicProjectile : MonoBehaviour
 
         Rigidbody.velocity = shootDirection * 20f;
         shootTime = Time.time;
+
+        if(target != null)
+        {
+            targetPositionHeightOffset = target.GetComponent<Collider>().bounds.center - target.position;
+        }
     }
 
     private void Update()
     {
+        if (target != null)
+        {
+            Vector3 targetPosition = target.position + targetPositionHeightOffset;
+            Vector3 targetDirection = targetPosition - transform.position;
+            if (Vector3.Angle(targetDirection, Rigidbody.velocity) < 15)
+            {
+                Rigidbody.velocity = Vector3.RotateTowards(Rigidbody.velocity, (targetPosition - transform.position).normalized, 3f * Time.deltaTime, 0);
+            }
+        }
+
         Rigidbody.velocity += ((Rigidbody.velocity * 0.1f) * Time.deltaTime);
 
         if (Time.time - shootTime > 5f)
@@ -77,8 +99,8 @@ public class MagicProjectile : MonoBehaviour
             if (health != null)
             {
                 health.TakeDamage(40);
-                
-                if(health.MovingCharacter != null)
+
+                if (health.MovingCharacter != null)
                 {
                     health.MovingCharacter.WasAttacked(impactPoint + (shootingTransform.position - health.transform.position).normalized * 0.5f, shootingTransform, 5);
                 }
