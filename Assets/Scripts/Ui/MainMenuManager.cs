ba8fd54e97f4dbf39933e63ff6c8a42d2f054a3e
Adam Tovatt <sporedenstore@hotmail.com>
2022-02-04 18:06:22 +0100

    remove skybox from main menu

--- Assets/Scripts/Ui/MainMenuManager.cs
+++ Assets/Scripts/Ui/MainMenuManager.cs
@@ -7,22 +7,8 @@ public class MainMenuManager : MonoBehaviour
     public Transform MainCameraSpawnPoint;
     public GameObject MainCameraPrefab;
 
-    private Spawner skyboxSpawner;
     private SkyboxCamera skyboxCamera;
     private Transform mainCameraInstance;
-
-    private void Awake()
-    {
-        skyboxSpawner = gameObject.GetComponent<Spawner>();
-        skyboxSpawner.OnObjectSpawned += (sender, spawnedObject) =>
-        {
-            skyboxCamera = spawnedObject.GetComponentInChildren<SkyboxCamera>();
-            if (mainCameraInstance != null)
-            {
-                skyboxCamera.SetMainCamera(mainCameraInstance);
-            }
-        };
-    }
 
     private void Start()
     {
